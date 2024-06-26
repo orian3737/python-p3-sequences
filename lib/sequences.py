fe17e8bdd2f0e1fa4bcae314e81ca3964f282cae
Ryan Murzyn <orian3737@gmail.com>
2024-06-26 17:12:42 -0400

    ok

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fibonacci_sequence = [0]
+    if length > 1:
+        fibonacci_sequence.append(1)
+    
+    for i in range(2, length):
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
+
+
+if __name__ == "__main__":
+    print_fibonacci(10) 
